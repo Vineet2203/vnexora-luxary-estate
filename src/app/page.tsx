e46dc5bd51ac93c91b1294b38377a58496582982
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-06-06 00:25:27 +0530

    about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "../components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <About />
+      {/* Add other sections here one by one */}
+      {/* Example: <Services /> */}
+      {/* Example: <Gallery /> */}
+      {/* Example: <Contact /> */}
       {/* Add other sections here one by one */}
       {/* Example: <About /> */}
       {/* Example: <Services /> */}
