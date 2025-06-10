73635ee6af2ae65ee725582ea333371d25fd6b6b
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-06-10 12:33:51 +0530

    inititaed the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@ import Hero from "../components/Hero";
 import AboutUs from "../components/AboutUs";
 import WhyChooseUs from "../components/WhyChoose";
 import Gallery from "../components/Gallery";
+<<<<<<< Updated upstream
 import OurAllies from '../components/OurAllies';
 import Services from "../components/Services";
+=======
+import Footer from "../components/Footer";
+
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
@@ -14,9 +19,31 @@ export default function Home() {
       <AboutUs />
       <WhyChooseUs />
       <Gallery />
+<<<<<<< Updated upstream
       <OurAllies />
       <Services />
 
+=======
+      <Footer />
+    
+      {/* Add other sections here one by one */}
+      {/* Example: <About /> */}
+      {/* Example: <Services /> */}
+      {/* Example: <Gallery /> */}
+      {/* Example: <Contact /> */}
+    
+      {/* Add other sections here one by one */}
+      {/* Example: <About /> */}
+      {/* Example: <Services /> */}
+      {/* Example: <Gallery /> */}
+      {/* Example: <Contact /> */}
+      {/* Add other sections here one by one */}
+      {/* Example: <About /> */}
+      {/* Example: <Services /> */}
+      {/* Example: <Gallery /> */}
+      {/* Example: <Contact /> */}
+    
+>>>>>>> Stashed changes
       {/* Add other sections here one by one */}
       {/* Example: <Services /> */}
       {/* Example: <Gallery /> */}
