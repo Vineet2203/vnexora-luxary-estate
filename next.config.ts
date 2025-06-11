b208887bb0139df1cfdf54352d59d8e916534821
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-06-11 11:28:41 +0530

    added all the images of the allies

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,32 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
  images: {
-    domains: ['hospitality-on.com'], // 
+    domains: ['hospitality-on.com',
+      'www.asiamiles.com',
+      'globalhospitalitysolution.com',
+      'mir-s3-cdn-cf.behance.net',
+      'www.parksidehotels.com',
+      'encrypted-tbn0.gstatic.com',
+      'seeklogo.com',
+      'svgmix.com',
+      'upload.wikimedia.org',
+      'cdn.worldvectorlogo.com',
+      'cardinalhotel.com',
+      'logowik.com',
+      'res.cloudinary.com',
+      'www.salesforce.com',
+      'img-cdn.publive.online',
+      'images-platform.99static.com',
+      'play-lh.googleusercontent.com',
+      'safariplus.co.in',
+      'api.blessingsonthenet.com',
+      'www.tnhglobal.com',
+      'upload.wikimedia.org',
+      'goeastofedmonton.com',
+      'digital.ihg.com'
+
+    ], 
+
   },
 };
 
