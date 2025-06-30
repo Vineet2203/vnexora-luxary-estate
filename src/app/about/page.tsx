b6324f43a5a62add95a14c4b229f7f38b1e4abe3
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-06-30 12:17:39 +0530

    updated about us page with font

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,13 @@
 
 import React, { useState } from 'react';
 import Card from '@/components/common/Card';
+import { Poppins, Playfair_Display } from 'next/font/google';
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400'] });
+const playfair = Playfair_Display({ subsets: ['latin'], weight: ['700'] });
 
 const AboutPage = () => {
   const [activeTab, setActiveTab] = useState<'vision' | 'mission' | 'purpose'>('vision');
-
   const teamMembers = [
     {
       name: "Pooja Tripathi",
@@ -115,8 +118,8 @@ const AboutPage = () => {
     },
   ];
 
-  return (
-    <main className="w-full mx-auto space-y-16 px-0 pb-10 relative bg-[#f7f5f1]">
+ return (
+    <main className={`w-full mx-auto space-y-16 px-0 pb-10 relative bg-[#f7f5f1] ${poppins.className}`}>
 
       {/* Hero Section */}
       <section className="relative w-full h-[70vh] flex items-center justify-center overflow-hidden">
@@ -129,7 +132,7 @@ const AboutPage = () => {
           className="absolute w-full h-full object-cover"
         />
         <div className="absolute text-center text-[#f5e8d7] z-10">
-          <h1 className="text-4xl md:text-5xl font-extrabold">ABOUT VNEXORA</h1>
+          <h1 className={`text-4xl md:text-5xl font-extrabold ${playfair.className}`}>ABOUT VNEXORA</h1>
           <p className="mt-4 text-lg md:text-xl max-w-2xl mx-auto">
             Crafting Tailored Hospitality Solutions with Vision, Innovation & Expertise.
           </p>
@@ -138,23 +141,23 @@ const AboutPage = () => {
 
       {/* Who We Are */}
       <section id="who" className="max-w-7xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-4 text-[#432c15] text-center mt-15">Who We Are</h2>
+        <h2 className={`text-3xl font-bold mb-4 text-[#432c15] text-center mt-15 ${playfair.className}`}>Who We Are</h2>
         <p className="text-lg text-gray-600 text-justify">
-          Founded in April 2025 by IIT BHU alumnus Vineet Mishra, VNEXORA Luxury Estate is a Varanasi-based premium hospitality management and consulting firm. We specialize in transforming hotels and resorts into profitable, branded destinations through strategic partnerships and comprehensive services. From daily operations and revenue optimization to branding, HR, and guest experience innovation, we deliver tailored solutions that elevate every property we touch. Backed by deep industry expertise and global partnerships—from luxury giants to boutique brands—we empower property owners to unlock their full potential while crafting unforgettable guest journeys. At VNEXORA, we don’t just manage hotels—we redefine hospitality excellence.
+             Founded in April 2025 by IIT BHU alumnus Vineet Mishra, VNEXORA Luxury Estate is a Varanasi-based premium hospitality management and consulting firm. We specialize in transforming hotels and resorts into profitable, branded destinations through strategic partnerships and comprehensive services. From daily operations and revenue optimization to branding, HR, and guest experience innovation, we deliver tailored solutions that elevate every property we touch. Backed by deep industry expertise and global partnerships—from luxury giants to boutique brands—we empower property owners to unlock their full potential while crafting unforgettable guest journeys. At VNEXORA, we don’t just manage hotels—we redefine hospitality excellence.
         </p>
       </section>
 
       {/* Founder */}
-      <h2 className="text-3xl font-bold text-[#432c15] mb-4 text-center">Meet Our Founder</h2>
+      <h2 className={`text-3xl font-bold text-[#432c15] mb-4 text-center ${playfair.className}`}>Meet Our Founder</h2>
       <section className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-start gap-6">
         <div className="w-full md:w-2/3">
           <p className="text-lg text-gray-600 text-base leading-relaxed mb-4 mt-10 text-justify">
-            Mr. Vineet Mishra, the visionary behind Vnexora Luxury Estate Private Limited, brings with him over 15 years of rich experience in the hospitality and real estate industry. As an alumnus of the prestigious <strong>IIT (B.H.U), Varanasi</strong>, he combines the analytical rigor and technical acumen of an engineer with the creative foresight of an industry leader.
+              Mr. Vineet Mishra, the visionary behind Vnexora Luxury Estate Private Limited, brings with him over 15 years of rich experience in the hospitality and real estate industry. As an alumnus of the prestigious <strong>IIT (B.H.U), Varanasi</strong>, he combines the analytical rigor and technical acumen of an engineer with the creative foresight of an industry leader.
           </p>
           <p className="text-lg text-gray-600 text-base leading-relaxed mb-4 mt-10 text-justify">
             Born and raised in Varanasi, Mr. Mishra’s deep-rooted understanding of both traditional values and modern business dynamics uniquely positions him to bridge global standards with local relevance.
           </p>
-          <p className="text-lg text-gray-600 text-base leading-relaxed mb-4 mt-10 text-justify">
+           <p className="text-lg text-gray-600 text-base leading-relaxed mb-4 mt-10 text-justify">
             Under his leadership, Vnexora has emerged as a disruptive force — offering a new paradigm in property management, hotel operations, and strategic consulting.
           </p>
         </div>
@@ -174,7 +177,7 @@ const AboutPage = () => {
 
       {/* Team Section */}
       <section id="team" className="max-w-7xl mx-auto px-4 py-16">
-        <h2 className="text-3xl font-bold mb-4 text-[#432c15] text-center">Our Diverse Team</h2>
+        <h2 className={`text-3xl font-bold mb-4 text-[#432c15] text-center ${playfair.className}`}>Our Diverse Team</h2>
         <p className="text-lg text-gray-600 text-center mb-10">
           Our team blends tradition and innovation—comprising architects, interior designers, and tech professionals.
         </p>
@@ -205,44 +208,41 @@ const AboutPage = () => {
         </div>
 
         <div className="w-full lg:w-1/2 text-center lg:text-left space-y-4">
-          <h2 className="text-4xl font-extrabold tracking-wider text-[#432c15]">Philosophy</h2>
+          <h2 className={`text-4xl font-extrabold tracking-wider text-[#432c15] ${playfair.className}`}>Philosophy</h2>
 
           <div className="space-y-4">
             {["vision", "mission", "purpose"].map((key, idx) => (
               <div key={key}>
                 <button
                   onClick={() => setActiveTab(key as any)}
-                  className={`flex justify-between items-center w-full px-6 py-3 text-lg font-[serif] transition rounded-2xl ${
+                  className={`flex justify-between items-center w-full px-6 py-3 text-lg transition rounded-2xl ${
                     activeTab === key
                       ? "bg-white shadow text-[#1e1e1e]"
                       : "bg-transparent text-[#1e1e1e]"
                   }`}
                 >
-                  <span>
+                  <span className={`${poppins.className}`}>
                     {key === "vision" && "Our Vision"}
                     {key === "mission" && "Our Mission"}
                     {key === "purpose" && "Our Purpose"}
                   </span>
-                  <span className="text-[#b48578] font-semibold text-xl">{`0${idx + 1}`}</span>
+                  <span className="text-[#432c15] font-semibold text-xl">{`0${idx + 1}`}</span>
                 </button>
                 {activeTab === key && (
                   <div className="bg-white text-left text-slate-600 text-base px-6 py-4 rounded-2xl shadow-sm transition-all duration-300">
                     {key === "vision" && (
                       <p>
-                        To be a well reputed boutique hospitality management company operating in growing & emerging destinations worldwide.
-                        To be the most inspiring brand in hospitality for guests, team members and stakeholders alike.
+                        To be a well reputed boutique hospitality management company operating in growing & emerging destinations worldwide...
                       </p>
                     )}
                     {key === "mission" && (
                       <p>
-                        To deliver authentic, memorable guest experiences in beautifully designed spaces with a commitment to operational excellence,
-                        sustainability, and local connection.
+                        To deliver authentic, memorable guest experiences...
                       </p>
                     )}
                     {key === "purpose" && (
                       <p>
-                        To provide our guests with a transformative experience worth sharing by delighting all their senses.
-                        Our guests always leave with a truly engaging experience worth sharing.
+                        To provide our guests with a transformative experience worth sharing...
                       </p>
                     )}
                   </div>
